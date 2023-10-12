2ec76dee071f299b99ffb9c6f63b3d88731ad7d7
korenkovaelena <korenkovalena@gmail.com>
2023-10-12 17:08:55 +0300

    output changed

--- src/Main.java
+++ src/Main.java
@@ -15,12 +15,12 @@ public class Main {
     public static int readFromFile(String fileName) throws FileNotFoundException {
         Scanner sc = new Scanner(new File(fileName));
         int k = sc.nextInt(); //Кол-во дней
-        System.out.println("Количество дней = "+k);
+        //System.out.println("Количество дней = "+k);
         return k;
     }
     public static String[] trackRearranging(int k) throws FileNotFoundException {
         String[] arr = {"G", "C", "V"}; //Герань, крокус, фиалка
-        System.out.println(Arrays.toString(arr));
+       // System.out.println(Arrays.toString(arr));
         for (int i = 0; i<k; i++) {
             String tmp = arr[1];
             arr[1] = arr[2];
@@ -29,12 +29,21 @@ public class Main {
             arr[1] = arr[0];
             arr[0]=tmp;
         }
-        System.out.println(Arrays.toString(arr));
+       // System.out.println(Arrays.toString(arr));
         return arr;
     }
-    public static void writeToFile( String[] arr) throws IOException {
-        String outputFName = "FlowersOrderAfterKDays";
-        String s = Arrays.toString(arr);
+    public static void writeToFile( String[] str) throws IOException {
+        String outputFName = "OUTPUT.TXT";
+        String s = printArr(str);
         Files.writeString(Path.of(outputFName),s);
+    }
+    public static String printArr(String[] arr)
+    {String str = "";
+        for (int i = 0; i < arr.length; i++) {
+             str = str + (String) arr[i];
+
+
+        }
+        return str;
     }
 }
